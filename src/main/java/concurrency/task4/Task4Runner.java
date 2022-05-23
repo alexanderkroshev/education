ada74044b49a5641bade54c339bd64ac59d568b1
krosh95 <alexander_kroshev@mail.ru>
2022-05-23 17:13:07 +0700

    task 4 finished

--- src/main/java/concurrency/task4/Task4Runner.java
+++ src/main/java/concurrency/task4/Task4Runner.java
@@ -3,9 +3,10 @@ package concurrency.task4;
 import lombok.SneakyThrows;
 
 public class Task4Runner {
+
     @SneakyThrows
     public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 10; i++) {
             new Thread(new Car(i)).start();
             Thread.sleep(200);
         }
---
+++ src/main/java/concurrency/task5/Ferry.java
@@ -0,0 +1,4 @@
+package concurrency.task5;
+
+public class Ferry {
+}
